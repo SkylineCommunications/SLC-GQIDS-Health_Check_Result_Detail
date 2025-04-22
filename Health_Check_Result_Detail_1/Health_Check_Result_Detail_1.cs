c24b95c1ba4d4870e62470633f9fd2492d38f7d0
Daniela-Prada <daniela.prada@skyline.be>
2025-04-22 14:46:30 -0500

    Changed folder location

--- Health_Check_Result_Detail_1/Health_Check_Result_Detail_1.cs
+++ Health_Check_Result_Detail_1/Health_Check_Result_Detail_1.cs
@@ -46,6 +46,8 @@ Revision History:
 DATE		VERSION		AUTHOR			COMMENTS
 
 03/09/2024	1.0.0.1		DPR, Skyline	Initial version
+11/09/2024	1.0.0.2		DPR, Skyline	QA
+22/04/2025	1.0.0.3		DPR, Skyline	Changed folder location
 ****************************************************************************
 */
 
